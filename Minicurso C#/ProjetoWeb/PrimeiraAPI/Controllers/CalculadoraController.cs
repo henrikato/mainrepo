26b9c47028486069bb805b031b8cb9639b21a8a0
Henrique T. kato <henrique.kato@etec.sp.gov.br>
2018-03-08 21:22:53 -0300

    Initial commit

---
+++ Minicurso C#/ProjetoWeb/PrimeiraAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpPost]
+        public double Somar([FromBody] Dictionary<string, string> valores)
+        {
+            var numero1 = double.Parse(valores["numero1"]);
+            var numero2 = double.Parse(valores["numero2"]);
+            return numero1 + numero2;
+        }
+    }
+}
