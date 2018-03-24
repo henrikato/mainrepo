ebe0371e43cd8460cf9587804bc432e158e05005
Henrique T. kato <henrique.kato@etec.sp.gov.br>
2018-03-24 20:57:16 -0300

    Aula do dia 23/03

---
+++ Minicurso C#/ProjetoWeb/ConsoleApplication/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    public class Calculadora
+    {
+        public Calculadora()
+        {
+            Console.WriteLine("Iniciando a Calculadora...");
+        }
+
+        public double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public void Alertar(double x)
+        {
+            Console.WriteLine("O valor de X e: {0}", x);
+        }
+    }
+}
--- Minicurso C#/ProjetoWeb/ConsoleApplication/Program.cs
+++ Minicurso C#/ProjetoWeb/ConsoleApplication/Program.cs
@@ -6,12 +6,21 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int x;
-            x = int.Parse(Console.ReadLine());
-            var nome = Console.ReadLine();
-            Console.WriteLine("Seu nome é {1}. E você digitou o número {0}.", x, nome);
-            Console.ReadLine();
+            var a = new Aluno(); // chama o construtor
+            a.Nome = "Rodrigo Gonçalves"; // chama o método set da propriedade Nome
+            a.DataNascimento = new DateTime(1998, 1, 5); // chama o método set da propriedade DataNascimento
+            Console.WriteLine($"A idade de { a.Nome} { a.Idade}");
+            
+            //var cal = new Calculadora();
+            //var x = cal.Somar(10, 5);
+            //cal.Alertar(x);
+
+            //Console.WriteLine("Hello World!");
+            //int x;
+            //x = int.Parse(Console.ReadLine());
+            //var nome = Console.ReadLine();
+            //Console.WriteLine("Seu nome é {1}. E você digitou o número {0}.", x, nome);
+            //Console.ReadLine();
         }
     }
 }
